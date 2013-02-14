3afd3ad02637f4c2b2fbcec4ccdd46f909f4d38e
Yannick <ycomte@wanadev.fr>
2013-02-14 14:03:47 +0100

    Work on tilemap

--- Samples/src/atlantis/samples/tilemap/TilemapGame.java
+++ Samples/src/atlantis/samples/tilemap/TilemapGame.java
@@ -4,16 +4,30 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import atlantis.engine.graphics.Entity;
 import atlantis.engine.graphics.Sprite;
+import atlantis.engine.graphics.tilemap.Layer;
 import atlantis.framework.Game;
 import atlantis.framework.GameTime;
+import atlantis.framework.graphics.Texture2D;
 import atlantis.input.KeyboardComponent;
 
 public class TilemapGame extends Game {
-
+	private Layer layer;
 	
 	public TilemapGame() {
 		super(1024, 600, "Atlantis Game Engine for Java - Tilemap Sample");
-
+		
+		int [][] tiles = {
+			{0, 0, 0, 0, 0, 0},
+			{0, 1, 1, 1, 1, 0},
+			{0, 1, 1, 1, 1, 0},
+			{0, 1, 1, 1, 1, 0},
+			{0, 0, 0, 0, 0, 0},
+		};
+		
+		Texture2D tileset = this.getContentManager().loadTexture("tileset.png");
+		
+		layer = new Layer(tileset, tiles);
+		
 	}
 	
 	public void loadContent() {
@@ -29,7 +43,7 @@ public class TilemapGame extends Game {
 	
 	public void draw(Graphics graphics) {
 		super.draw(graphics);
-
+		layer.draw(graphics);
 	}
 
 	public static void main(String [] args) {
